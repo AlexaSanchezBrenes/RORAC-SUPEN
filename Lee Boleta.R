7ee4c8737a253fda0ff6d66158f07477e126bb99
Alexa <sanchezbrenes17@gmail.com>
2020-08-17 21:16:15 -0600

    Versión 2: corrección títulos

--- Lee Boleta.R
+++ Lee Boleta.R
@@ -1,27 +1,29 @@
+#Recibe un txt o csv con la informacion de la emision para un periodo dado.
 lee.boleta<<-function(archivo){
   library(readr)
   library(stringr)
   library(dplyr)
   
   tbl<-as.data.frame(unclass(read.table(archivo, 
-                                        header = TRUE, sep = ",",
-                                        fileEncoding = "UTF-8"))) %>% 
-    mutate(Mercado.de.Negociaci?n = as.character( unlist(Mercado.de.Negociaci?n)),
-           Nemot?cnico.del.Emisor = as.character(unlist(Nemot?cnico.del.Emisor)),
-           Nemot?cnico.del.instrumento = as.character(unlist(Nemot?cnico.del.instrumento)),
+                                        header = TRUE, sep = ","))) %>% 
+    mutate(Mercado.de.Nego = as.character( unlist(tbl[,3])),
+           Emisor = as.character(unlist(tbl[,4])),
+           Instrumento = as.character(unlist(tbl[,5])),
            Tasa.facial = as.numeric(str_replace(Tasa.facial, ",", ".")),
            Precio = as.numeric(str_replace(Precio, ",", ".")),
            Valor.Transado = as.numeric(str_replace(Valor.Transado, ",", ".")),
            Valor.facial = as.numeric(str_replace(Valor.facial, ",", ".")),
            Tis = as.numeric(str_replace(Tis, ",", ".")),
            Tir = as.numeric(str_replace(Tir, ",", ".")),
-           Fecha.de.Operaci?n = as.Date(as.character.Date(Fecha.de.Operaci?n)),
+           Fecha.de.Operacion = as.Date(as.character.Date(tbl[,15])),
            Fecha.de.Vencimiento = as.Date(as.character.Date(Fecha.de.Vencimiento)),
-           Plazo.de.la.operaci?n = as.character(Plazo.de.la.operaci?n),
+           Plazo.Operacion= as.character(tbl[,21]),
            Rendimiento.de.la.recompra = as.numeric(str_replace(Rendimiento.de.la.recompra , ",", ".")),
            Moneda.del.instrumento = as.character(unlist(Moneda.del.instrumento)),
-           Moneda.de.liquidaci?n = as.character(unlist( Moneda.de.liquidaci?n)),
-           Operaci?n.Cruzada = as.character(unlist( Operaci?n.Cruzada)),
-           Recompra = as.character(unlist( Recompra)))
+           Moneda.de.liquidacion = as.character(unlist( tbl[,27])),
+           Operacion.Cruzada = as.character(unlist( tbl[,28])),
+           Recompra = as.character(unlist( Recompra))) %>% 
+    select(-c(3,4,5,15,21,27,28))
+  
   return(as.data.frame(tbl))
 }
