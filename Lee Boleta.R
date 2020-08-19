36cf84854213a54fc46b07bd890161c794c8e04d
Alexa <sanchezbrenes17@gmail.com>
2020-08-18 20:40:45 -0600

    Versión con encoding correcto

--- Lee Boleta.R
+++ Lee Boleta.R
@@ -3,35 +3,26 @@ lee.boleta<<-function(archivo){
   library(readr)
   library(stringr)
   library(dplyr)
-  
+  options(stringsAsFactors = FALSE)
 if(substring(archivo, 1, 6) =="Boleta"){ 
   tbl<-as.data.frame(unclass(read.table(archivo, 
-                                                header = TRUE, sep = ",", 
-                                                fileEncoding = "UTF-8")))
+                                        header = TRUE, 
+                                        sep = ",", 
+                                        encoding =  "UTF-8",
+                                        stringsAsFactors = FALSE)))
  }else{
    tbl<- as.data.frame(unclass(read.table(archivo, 
-                                        header = TRUE, sep = ",")))
+                                          header = TRUE, 
+                                          sep = ",", 
+                                          encoding = "Latin1",
+                                          stringsAsFactors = F)))
  }
   
+  names<-stringi::stri_trans_general(colnames(tbl), "Latin-ASCII")
+  colnames(tbl)<-names
   
-  
-               
   df<-tbl%>% 
-    rename(Numero.de.Contracto = "Número.de.Contrato",
-           Numero.de.Contracto.Largo = "Número.de.Contrato.Largo",
-           Mercado.de.Negociacion = "Mercado.de.Negociación",
-           Nemotecnico.del.Emisor = "Nemotécnico.del.Emisor",
-           Nemotecnico.del.instrumento = "Nemotécnico.del.instrumento",
-           Fecha.de.Operacion = "Fecha.de.Operación",
-           Fecha.de.Renovacion = "Fecha.de.Renovación",
-           Fecha.Ultimo.Pago.Intereses = "Fecha.Último.Pago.Intereses",
-           Plazo.de.la.operacion = "Plazo.de.la.operación",
-           Moneda.de.liquidacion = "Moneda.de.liquidación",
-           Operacion.Cruzada = "Operación.Cruzada") %>% 
-   mutate(Mercado.de.Negociacion = as.character( unlist(Mercado.de.Negociacion)),
-           Nemotecnico.del.Emisor  = as.character(unlist(Nemotecnico.del.Emisor)),
-           Nemotecnico.del.instrumento = as.character(unlist(Nemotecnico.del.instrumento)),
-           Tasa.facial = as.numeric(str_replace(Tasa.facial, ",", ".")),
+   mutate(Tasa.facial = as.numeric(str_replace(Tasa.facial, ",", ".")),
            Precio = as.numeric(str_replace(Precio, ",", ".")),
            Valor.Transado = as.numeric(str_replace(Valor.Transado, ",", ".")),
            Valor.facial = as.numeric(str_replace(Valor.facial, ",", ".")),
@@ -40,11 +31,9 @@ if(substring(archivo, 1, 6) =="Boleta"){
            Fecha.de.Operacion = as.Date(as.character.Date(Fecha.de.Operacion)),
            Fecha.de.Vencimiento = as.Date(as.character.Date(Fecha.de.Vencimiento)),
            Plazo.de.la.operacion = as.character(Plazo.de.la.operacion),
-           Rendimiento.de.la.recompra = as.numeric(str_replace(Rendimiento.de.la.recompra , ",", ".")),
-           Moneda.del.instrumento = as.character(unlist(Moneda.del.instrumento)),
-           Moneda.de.liquidacion = as.character(unlist( Moneda.de.liquidacion)),
-           Operacion.Cruzada = as.character(unlist( Operacion.Cruzada)),
-           Recompra = as.character(unlist( Recompra))) 
+           Rendimiento.de.la.recompra = as.numeric(str_replace
+                                        (Rendimiento.de.la.recompra , ",", ".")))
+ 
   
   return(as.data.frame(df))
 }
