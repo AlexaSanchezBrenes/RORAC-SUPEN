52aa0eed682c410a8c92c37f722825b3b3893e01
Alexa <sanchezbrenes17@gmail.com>
2020-08-19 20:59:24 -0600

    Función para generar una lista con la información de todas las boletas que se ubican en una carpeta

--- Lista DF Boletas.R
+++ Lista DF Boletas.R
@@ -1,20 +1,20 @@
-#Crea lista de data frames con las boletas
+#Input: dirección ed la carpeta donde se encuentran las boletas
+#Output: Lista con las boletas y los cuadros resumen
 lista.df.boletas<-function(path="~/RORAC SUPEN/Emisiones/Boletas"){
  
   folder<-list.files(path,full.name = TRUE )
   n<-length(folder)
   archivo<-numeric(n)
   lista.df<-list()
-  lista.resumenes<-list()
-  
+
   for(i in 1:n){
     if(length(list.files(path = folder[i], pattern = ".txt")) != 0){
       archivo[i]<- list.files(path = folder[i], pattern = ".txt")
        }else{
       archivo[i]<- list.files(path = folder[i], pattern = ".csv")}
     
-     lista.df[[i]]<-lee.boleta(paste0(folder[i],"/",archivo[i]))
-     #lista.resumenes[[i]]<-resumen.boleta(lista.df[[i]])
+     #lista.df[[i]]<-lee.boleta(paste0(folder[i],"/",archivo[i]))
+     lista.df[[i]]<-resumen.boleta(paste0(folder[i],"/",archivo[i]))
   }
   
  names(lista.df)<-archivo  
