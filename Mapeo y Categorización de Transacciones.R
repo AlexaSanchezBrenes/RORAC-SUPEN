c1b2246a94e4e95fd6cad0af143ab1e5a770e070
DesAct12 <isaac.actuario@gmail.com>
2020-10-08 08:08:44 -0600

    Se crea el mapeo y caracteización de títulos.

---
+++ Mapeo y Categorización de Transacciones.R
@@ -0,0 +1,167 @@
+#              
+#                    Consultoría RORAC-SUPEN 
+#                 Mapeo y categorización de las transacciones 
+
+# Autores:
+# Alexa Sánchez
+# Laura Campos
+# Isaac Z. Arias
+
+# El siguiente módulo realiza un mapeo de las transacciones, con el fin de categorizar 
+#la información
+
+# Paquete necesarios:
+library(readr)
+library(stringi)
+library(dplyr)
+library("tools")
+library(stringr)
+library(lubridate)
+library(ggplot2)
+options(stringsAsFactors = FALSE)
+
+# Dirrección de los datos:
+Dir <- "~/RORAC SUPEN/Transacciones"
+
+# Función para leer los datos
+
+#Input: archivos .txt que contienen la información de las transacciones
+lee.datos<<-function(archivo){
+  
+  tbl<- as.data.frame(unclass(read.table(archivo, 
+                                         header = TRUE, 
+                                         encoding = "Latin1",
+                                         stringsAsFactors = F)))
+  return(tbl)
+}
+
+# Función para obtener los datos de las carpetas
+
+#Input: dirección ed la carpeta donde se encuentran los archivos con las transacciones
+#Output: Lista con todas las transacciones
+lista.df<-function(path=Dir){
+  
+  folder<-list.files(path,full.name = TRUE )
+  n<-length(folder)
+  lista.df<-list()
+  
+  for(i in 1:n){
+    lista.df[[i]] <-lee.datos(folder[i])
+  }
+  return(lista.df)
+}
+
+transacciones <- lista.df(Dir)
+names.1 <- colnames(transacciones[[14]])
+names.2 <- colnames(transacciones[[1]])
+
+# Columnas que se agregan a la data a partir de Enero 2020
+col.nuevas <- as.data.frame(names.1[which(!names.1%in%names.2)])
+col.seleccion <- names.1[which(names.1%in%names.2)]
+
+## Se seleccionan las columnas en común dados los dos grupos de información
+transacciones.2 <- lapply(transacciones, 
+                          function(x) x%>% select(col.seleccion))
+transacciones.2 <- do.call("rbind", transacciones.2)
+
+
+## Se seleccionan las columnas en común dados los dos grupos de información
+transacciones.2 <- lapply(transacciones, 
+                          function(x) x%>% select(all_of(col.seleccion)))
+transacciones.2 <- do.call("rbind", transacciones.2)
+
+## 1.1) Agrupación por Código de la modalidad de inversión: 
+# clase de título valor o instrumento bursátil que se está reportando
+
+cod.mod.inv <- transacciones.2 %>% 
+  group_by(FEC_DAT, COD_MOD_INV, COD_MON) %>% 
+  summarise(Conteo  = n()) 
+
+prom.mensual.cod.mod.inv <- cod.mod.inv %>% 
+  group_by(Anno = year(FEC_DAT),
+           Mes = month(FEC_DAT),
+           COD_MOD_INV, COD_MON) %>% 
+  summarise(PromedioRegistrosMensuales = mean(Conteo)) 
+
+## Gráfica del comportamiento histórico de las modalidad de inversión:
+
+# i. De acuerdo con la documentación (Tablas y Validac RC V6, pag 80) 
+# los tipos de inversiones se agrupan en 10 tipos.
+# ii .A partir de 2020 (ver Tablas y Validac V5, pag 10 rige un cambio 
+# en la clasificación)
+
+#--------------DUDA------------------------------------------
+#-----ENTENDER COMO SE REAGRUPARON LAS MODALIDADES-----------
+#-----ENTENDER DIFERENCIA ENTRE DEUDA INDIVIDUAL Y ESTANDARIZADA SEGÚN EL NIVEL---
+#-----Ver como se van a agrupar esas clasificaciones------------
+
+prom.mensual.cod.mod.inv$Fecha = paste(prom.mensual.cod.mod.inv$Anno,
+                                       prom.mensual.cod.mod.inv$Mes,
+                                       "01", sep="-") %>%
+  ymd() %>%
+  as.Date()
+# Colones
+prom.mensual.cod.mod.inv %>% filter(COD_MON == 1) %>% 
+  ggplot(aes(Fecha, PromedioRegistrosMensuales)) + 
+  geom_col(aes(fill = COD_MOD_INV)) + 
+  scale_x_date(date_labels = "%b %Y")+ 
+  labs(title = "Promedio mensual de registros de las modalidades de inversión" )
+
+## Gráfica del comportamiento histórico para una modalidad de inversión específica:  
+
+#i. Agrupación por moneda
+g.prom.mensual.cod.mod.inv <- function(Modalidad){
+  prom.mensual.cod.mod.inv  %>% 
+    filter(COD_MOD_INV %in% Modalidad) %>% 
+    ggplot(aes(Fecha, PromedioRegistrosMensuales)) + 
+    geom_col(aes(fill = COD_MON)) + 
+    scale_x_date(date_labels = "%b %Y")+ 
+    labs(title = "Promedio mensual de registros por moneda " )
+}
+#ii. Colones
+g.prom.mensual.cod.mod.inv.COL <- function(Modalidad){
+  prom.mensual.cod.mod.inv  %>% filter(COD_MON == 1) %>%
+    filter(COD_MOD_INV %in% Modalidad) %>% 
+    ggplot(aes(Fecha, PromedioRegistrosMensuales)) + 
+    geom_col(aes(fill = COD_MOD_INV)) + 
+    scale_x_date(date_labels = "%b %Y")+ 
+    labs(title = "Promedio mensual de registros (CRC)" )
+}
+#iii. Dolares
+g.prom.mensual.cod.mod.inv.DOL <- function(Modalidad){
+  prom.mensual.cod.mod.inv  %>% filter(COD_MON ==2) %>%
+    filter(COD_MOD_INV %in% Modalidad) %>% 
+    ggplot(aes(Fecha, PromedioRegistrosMensuales)) + 
+    geom_col(aes(fill = COD_MOD_INV)) + 
+    scale_x_date(date_labels = "%b %Y")+ 
+    labs(title = "Promedio mensual de registros ($)" )
+}
+
+
+
+## Por moneda:
+Acciones <- g.prom.mensual.cod.mod.inv("AC")
+Acciones
+
+DeudaIndividual <- g.prom.mensual.cod.mod.inv("DI")
+DeudaIndividual
+
+
+## Colones:
+DeudaEstandarizada.COL <- g.prom.mensual.cod.mod.inv.COL(c("DE", "D2", "D3"))
+DeudaEstandarizada.COL
+
+ParticipacionFondos.COL <- g.prom.mensual.cod.mod.inv.COL(c("P1", "P2", "P3"))
+ParticipacionFondos.COL
+
+## Dolares:
+
+ExchangeTradedFunds.DOL <- g.prom.mensual.cod.mod.inv.DOL(c("E1", "E2", "E3"))
+ExchangeTradedFunds.DOL
+
+DeudaEstandarizada.DOL <- g.prom.mensual.cod.mod.inv.DOL(c("DE", "D2", "D3"))
+DeudaEstandarizada.DOL
+
+ParticipacionFondos.DOL <- g.prom.mensual.cod.mod.inv.DOL(c("P1", "P2", "P3"))
+ParticipacionFondos.DOL
+
