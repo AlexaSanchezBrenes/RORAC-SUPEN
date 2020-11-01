317f09d0ed5b280ac88c812a07de9351646f3468
DesAct12 <isaac.actuario@gmail.com>
2020-10-31 23:05:19 -0600

    Módulo terminado.

--- CurvaSoberanaMensual.R
+++ CurvaSoberanaMensual.R
@@ -5,11 +5,11 @@
 
 # Autores:
 # Alexa Sánchez
-# Laura Campos
 # Isaac Z. Arias
 
 # El siguiente módulo representa la función de error cuadrático medio que se debe minimizar para 
-# obtener los parámetros óptimos de la curva cero cupón para cada mes según Modelo de Nelson-Siegel.
+# obtener los parámetros óptimos de la curva cero cupón para cada mes según Modelo de Nelson-Siegel
+# y Svensson modificado.
 
 
 # Paquetes necesarios:
@@ -154,17 +154,6 @@ Tau.total <- function(fila){
   return(tabla)
 }
 
-# Función para calcular la TIR de un bono:
-TIR <- function(fila){
-  fila <- Tau.total(fila) %>% arrange(Fecha.Pago)
-  calculo.tir <- function(tir){
-    fila$Precio[1]-sum((rep(fila$Tasa.facial[1],length(fila$Tau))+c(rep(0,length(fila$Tau)-1),1))*(1+tir)^(-fila$Tau/12))
-  }
-  resultado <- nleqslv(0.05, calculo.tir)
-  fila <- fila %>% mutate(TIR = resultado$x)
-  return(fila)
-}
-
 # Utilizando la versión original del modelo Nelson-Siegel:
 
 # Función que debe ser minimizada para estimar parámetros usando diferencia máxima:
@@ -273,10 +262,7 @@ FuncionObjetivo.NS.Pon <- function(X){
 
 # Función que debe ser minimizada para estimar parámetros usando diferencia máxima:
 FuncionObjetivo.SA.Max <- function(X){
-<<<<<<< HEAD
 
-=======
->>>>>>> 276e96f9ab41d7b0b156871dc314e7de8cc0cbbb
   # Redefinimos parámetros:
   B0 <- X[1]
   B1 <- TRI.corta[i]-B0
@@ -408,10 +394,9 @@ lim.tl <- log(20/100+1)/12
 # Limite beta1:
 lim.beta <- log(0.5+1)/12
 
-# Calculamos la TIR para cada bono para observación:
-Tir.aplicado <- bind_rows(lapply(split(Lista.Bonos[[i]], seq(nrow(Lista.Bonos[[i]]))), TIR)) %>% group_by(Numero.de.Contrato.Largo) %>%
-  mutate(cant= n()) %>% ungroup() %>% filter(cant ==1) %>% 
-  select(-Fecha.Pago) %>% unique()
+# Betas Iniciales:
+Beta2Inicial <- 0
+Beta3Inicial <- 0
 
 # Calculamos los Taus para cada cero cupón:
 Tau.aplicado <- bind_rows(lapply(split(Lista.Bonos[[i]], seq(nrow(Lista.Bonos[[i]]))), Tau.total))
@@ -433,11 +418,7 @@ toc()
 
 #---------------------------------------- Prueba para Optimizadores:
 
-# Incializa parámetros:
-Beta2Inicial <- 0
-Beta3Inicial <- 0
-
-## Particle Swarm Optimization
+## Particle Swarm Optimization - Nelson Siegel
 
 tic()
 # Realizamos la optimización con función objetivo de Ponderación:
@@ -447,6 +428,9 @@ prueba.NS.pon.pso <- psoptim(par = c(TRI.larga, Beta2Inicial, (3+lim.n)/2),
                              upper = c(lim.tl, lim.beta, lim.n),
                              control = list(maxit = 1000,s = 15,w = -0.1832,c.p =0.5287,c.g = 3.1913))
 toc()
+
+## Particle Swarm Optimization - Svensson
+
 tic()
 # Realizamos la optimización con función objetivo de Ponderación:
 prueba.SA.pon.pso <- psoptim(par = c(TRI.larga, Beta2Inicial, Beta3Inicial, (3 + 2*12)/2, (lim.n+3*12)/2),
@@ -456,43 +440,34 @@ prueba.SA.pon.pso <- psoptim(par = c(TRI.larga, Beta2Inicial, Beta3Inicial, (3 +
                              control = list(maxit = 1000,s = 20,w = -0.1832,c.p =0.5287,c.g = 3.1913))
 toc()
 
+## Simulated Annealing - Nelson Siegel
 
-
-## Simulated Annealing
-
-tic()
-# Realizamos la optimización con función objetivo de Máximo:
-prueba.max.sa <- optim_sa(fun = FuncionObjetivo.Max,
-                          start = c(TRI.larga, Beta2Inicial, lim.n/2),
-                          trace = TRUE,
-                          lower = c(-2/100+TRI.larga, -5, 0),
-                          upper = c(2/100+TRI.larga, 5, lim.n),
-                          control = list(rf = c(0.95, 0.95, 0.95),
-                                         nlimit = 1000))
-toc()
 tic()
 # Realizamos la optimización con función objetivo de Ponderación:
-prueba.max.sa <- optim_sa(fun = FuncionObjetivo.Pon,
-                          start = c(TRI.larga, Beta2Inicial, lim.n/2),
+prueba.ns.pon <- optim_sa(fun = FuncionObjetivo.NS.Pon,
+                          start = c(TRI.larga, Beta2Inicial, (3+lim.n)/2),
                           trace = TRUE,
-                          lower = c(-2/100+TRI.larga, -5, 0),
-                          upper = c(2/100+TRI.larga, 5, lim.n),
-                          control = list(rf = c(0.95, 0.95, 0.95),
-                                         nlimit = 1000))
+                          lower = c(TRI.larga, -lim.beta, 3),
+                          upper = c(lim.tl, lim.beta, lim.n),
+                          control = list(nlimit = 1000))
+toc()
+
+## Simulated Annealing - Svensson
+
+# Realizamos la optimización con función objetivo de Ponderación:
+tic()
+prueba.SA.pon <- optim_sa(fun = FuncionObjetivo.SA.Pon,
+                          start = c(TRI.larga, Beta2Inicial, Beta3Inicial, (3 + 2*12)/2, (lim.n+3*12)/2),
+                          trace = TRUE,
+                          lower = c(TRI.larga, -lim.beta, -lim.beta, 3, 3*12),
+                          upper = c(lim.tl, lim.beta, lim.beta, 2*12, lim.n),
+                          control = list(nlimit = 1000))
 toc()
 
 ## Nelder-Mead - Nelson Siegel 
 
 A=matrix(c(1,0,0,0,1,0,0,0,1,-1,0,0,0,-1,0,0,0,-1),nrow = 6,byrow = T)
 B=c(TRI.larga*0.99, -lim.beta, 3,-lim.tl, -lim.beta, -lim.n)
-
-tic()
-# Realizamos la optimización con función objetivo de Máximo:
-prueba.NS.max.nm <-constrOptim(theta = c(TRI.larga, Beta2Inicial, (3+lim.n)/2),
-                            f = FuncionObjetivo.Max, grad = NULL, ui=A, 
-                            ci=B, control = list(maxit=1000),
-                            method = "Nelder-Mead")
-toc() 
 tic()
 # Realizamos la optimización con función objetivo de Ponderación:
 prueba.NS.pon.nm <- constrOptim(theta = c(TRI.larga, Beta2Inicial, (3+lim.n)/2), 
@@ -505,14 +480,6 @@ toc()
 
 A=matrix(c(1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,-1,0,0,0,0,0,-1,0,0,0,0,0,-1,0,0,0,0,0,-1,0,0,0,0,0,-1),nrow = 10,byrow = T)
 B=c(TRI.larga*0.99, -lim.beta, -lim.beta, 3, 3*12, -lim.tl, -lim.beta, -lim.beta, -2*12, -lim.n)
-
-tic()
-# Realizamos la optimización con función objetivo de Máximo:
-prueba.SA.max.nm <-constrOptim(theta = c(TRI.larga, Beta2Inicial, Beta3Inicial,(3+2*12)/2, (lim.n+3*12)/2),
-                            f = FuncionObjetivo.SA.Max, grad = NULL, ui=A, 
-                            ci=B, control = list(maxit=1000),
-                            method = "Nelder-Mead")
-toc() 
 tic()
 # Realizamos la optimización con función objetivo de Ponderación:
 prueba.SA.pon.nm <- constrOptim(theta = c(TRI.larga, Beta2Inicial, Beta3Inicial,(3+2*12)/2, (lim.n+3*12)/2), 
@@ -524,13 +491,6 @@ toc()
 ## Genetic Algorithm - Nelson Siegel
 
 tic()
-
-# Realizamos la optimización con función objetivo de Máximo:
-prueba.NS.max.ga <- ga(type = "real-valued",fitness = FuncionObjetivo.NS.Max, 
-                    lower = c(TRI.larga, -lim.beta, 3),
-                    upper = c(lim.tl, lim.beta, lim.n),maxiter=1000)
-toc() 
-tic()
 # Realizamos la optimización con función objetivo de Ponderación:
 prueba.NS.pon.ga <- ga(type = "real-valued",fitness = FuncionObjetivo.NS.Pon, 
                     lower = c(TRI.larga, -lim.beta, 3),
@@ -540,13 +500,6 @@ toc()
 ## Genetic Algorithm - Svensson
 
 tic()
-
-# Realizamos la optimización con función objetivo de Máximo:
-prueba.NS.max.ga <- ga(type = "real-valued",fitness = FuncionObjetivo.SA.Max, 
-                       lower = c(TRI.larga, -lim.beta, -lim.beta, 3, 3*12),
-                       upper = c(lim.tl, lim.beta, lim.beta, 2*12, lim.n),maxiter=1000)
-toc() 
-tic()
 # Realizamos la optimización con función objetivo de Ponderación:
 prueba.NS.pon.ga <- ga(type = "real-valued",fitness = FuncionObjetivo.SA.Pon, 
                        lower = c(TRI.larga, -lim.beta, -lim.beta, 3, 3*12),
@@ -554,6 +507,18 @@ prueba.NS.pon.ga <- ga(type = "real-valued",fitness = FuncionObjetivo.SA.Pon,
 toc()
 
 #---------------------------------------- Calibración de la Constante de Ponderación:
+
+# Inicialización del punto inicial de la calibración:
+Beta0Inicial <- TRI.larga
+Beta3Inicial <- 0
+Beta2Inicial <- 0
+Beta1Inicial <- 0
+lim.n <- 60
+lim.beta <- log(0.5+1)/12
+Eta1Inicial <- (3+2*12)/2
+Eta2Inicial <- (lim.n+3*12)/2
+lim.tl.sup <- log(20/100+1)/12
+lim.tl.inf <- Beta0Inicial*0.99
 
 # Función que calibrar visualiza los errores semanales por contante:
 Calibrar.error.SA <- function(alpha.cal){
@@ -615,20 +580,22 @@ Calibrar.error.SA <- function(alpha.cal){
     return(ErrorTotal)
   }
   
+  A=matrix(c(1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,-1,0,0,0,0,0,-1,0,0,0,0,0,-1,0,0,0,0,0,-1,0,0,0,0,0,-1),nrow = 10,byrow = T)
+  B=c(lim.tl.inf, -lim.beta, -lim.beta, 3, 3*12, -lim.tl.sup, -lim.beta, -lim.beta, -2*12, -lim.n)
+  
   # Realizamos la optimización con función objetivo de Ponderación:
-  calibracion.SA.pon.pso <- psoptim(par = c(TRI.larga, Beta2Inicial, Beta3Inicial, (3 + 2*12)/2, (lim.n+3*12)/2),
-                                    fn = FuncionObjetivo.SA.Pon.cal,
-                                    lower = c(TRI.larga, -lim.beta, -lim.beta, 3, 3*12),
-                                    upper = c(lim.tl, lim.beta, lim.beta, 2*12, lim.n),
-                                    control = list(maxit = 700,s = 15,w = -0.1832,c.p =0.5287,c.g = 3.1913))
+  calibracion.SA.pon.nm <- constrOptim(theta = c(Beta0Inicial, Beta2Inicial, Beta3Inicial, Eta1Inicial, Eta2Inicial), 
+                                        f = FuncionObjetivo.SA.Pon.cal, ui=A,
+                                        ci=B, control = list(maxit=1000),
+                                        method = "Nelder-Mead")
   
   # Redefinimos parámetros:
-  B0 <- calibracion.SA.pon.pso$par[1]
+  B0 <- calibracion.SA.pon.nm$par[1]
   B1 <- TRI.corta[i]-B0
-  B2 <- calibracion.SA.pon.pso$par[2]
-  B3 <- calibracion.SA.pon.pso$par[3]
-  n1 <- calibracion.SA.pon.pso$par[4]
-  n2 <- calibracion.SA.pon.pso$par[5]
+  B2 <- calibracion.SA.pon.nm$par[2]
+  B3 <- calibracion.SA.pon.nm$par[3]
+  n1 <- calibracion.SA.pon.nm$par[4]
+  n2 <- calibracion.SA.pon.nm$par[5]
   
   # Se recaluclan las diferencias para generar el error máximo con los mismos parámetros:
   DiferenciasPrecio.Max.cal <- Tau.aplicado %>% 
@@ -655,11 +622,14 @@ Calibrar.error.SA <- function(alpha.cal){
 }
 
 # Inicializamos variables para calibración:
-alphas <- seq(0,10,0.2)
-
+alphas <- seq(0,40, 2)
 lista.graf.cal <- list()
-for(i in 1:length(Lista.Bonos)){
- 
+
+for(i in 1:(length(Lista.Bonos)-1)){
+  
+  # Calculamos los Taus para cada cero cupón:
+  Tau.aplicado <- bind_rows(lapply(split(Lista.Bonos[[i]], seq(nrow(Lista.Bonos[[i]]))), Tau.total))
+  
   # Generamos puntos de 0 a 10:
   serie.cal <- data.frame()
   for(al in alphas){
@@ -668,15 +638,17 @@ for(i in 1:length(Lista.Bonos)){
   serie.cal <- serie.cal %>% mutate(Diff.semana = as.factor(Diff.semana))
   
   # Gráfico sobre errores por semana:
-  graf.cons <- ggplot(data = serie.cal, aes(x = alpha, y = Error.max, colour = Diff.semana)) +
+  graf.cal <- ggplot(data = serie.cal, aes(x = alpha, y = Error.max, colour = Diff.semana)) +
     geom_line(size = 0.7) + ylab("Error Máximo") +
     geom_point() +
     theme_light() +
-    labs(title = "Constante de Ponderación")
+    labs(title = "Constante de Ponderación",color = "Semana de
+       \r Operación")
 
   # Visualizamos:
-  lista.graf.cal[[i]] <- graf.cons
+  lista.graf.cal[[i]] <- graf.cal
 }
+
 #---------------------------------------- Creación de la Curva para cada Mes:
 
 # Función para generar una curva cero cupón con Nelson-Siegel:
@@ -708,7 +680,7 @@ Curva.NS <- function(X, fecha.inicial, fecha.final){
 
 # Función para generar una curva cero cupón con Svensson Alterado:
 Curva.SA <- function(X, fecha.inicial, fecha.final){
-  #X <- prueba.SA.max.pso$par
+
   # Redefinimos parámetros:
   B0 <- X[1]
   B1 <- TRI.corta[i]-B0
@@ -739,31 +711,50 @@ Curva.SA <- function(X, fecha.inicial, fecha.final){
 
 # Incializa lista de resultados:
 Curvas.Terminadas <- list()
+
+# Inicializacion de alpha ideal:
+alpha <- 5
+
+# Inicialización del punto inicial de la calibración anterior:
+Beta3Inicial <- 0
 Beta2Inicial <- 0
+Beta1Inicial <- 0
+
+# Inicialización Límite superior del eta1:
+lim.n <- 60
+
+# Inicialización Límite beta1:
+lim.beta <- log(0.5+1)/12
 
 # Optimizamos la función objetivo para cada mes en los datos:
 for (i in 1:length(Lista.Bonos)) {
 
   # Calculamos los Taus para cada cero cupón:
   Tau.aplicado <- bind_rows(lapply(split(Lista.Bonos[[i]], seq(nrow(Lista.Bonos[[i]]))), Tau.total))
-  
-  # Limite superior del eta_1:
-  lim.n <- Tau(ymd(min(Lista.Bonos[[i]]$Fecha.de.Operacion)),
-               ymd(max(Lista.Bonos[[i]]$Fecha.de.Vencimiento)))
-  
-  # Definimos el eta1 incial:
+
+  # Definimos parámetros inciales:
   if(i == 1){
-    Eta1Inicial <- lim.n/2
+    Eta1Inicial <- (3+2*12)/2
+    Eta2Inicial <- (lim.n+3*12)/2
+    Beta0Inicial <- TRI.larga
+    lim.tl.sup <- log(20/100+1)/12
+    lim.tl.inf <- Beta0Inicial*0.99
   }else{
-    Eta1Inicial <- parametros.definitivos$par[3]
+    Eta1Inicial <- parametros.definitivos$par[4]
+    Eta2Inicial <- parametros.definitivos$par[5]
+    Beta0Inicial <- parametros.definitivos$par[1]
+    lim.tl.sup <- (1+2/100)*Beta0Inicial
+    lim.tl.inf <- (1-2/100)*Beta0Inicial
   }
   
-  # Realizamos la optimización con función objetivo:
-  parametros.definitivos <- psoptim(par = c(TRI.larga, Beta2Inicial, Eta1Inicial),
-                                    fn = FuncionObjetivo.Pon,
-                                    lower = c(-2/100+TRI.larga, -5, 0+1e-9),
-                                    upper = c(2/100+TRI.larga, 5, lim.n),
-                                    control = list(maxit = 1000,s = 15,w = -0.1832,c.p =0.5287,c.g = 3.1913))
+  A=matrix(c(1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,-1,0,0,0,0,0,-1,0,0,0,0,0,-1,0,0,0,0,0,-1,0,0,0,0,0,-1),nrow = 10,byrow = T)
+  B=c(lim.tl.inf, -lim.beta, -lim.beta, 3, 3*12, -lim.tl.sup, -lim.beta, -lim.beta, -2*12, -lim.n)
+
+  # Realizamos la optimización con función objetivo de Ponderación:
+  parametros.definitivos <- constrOptim(theta = c(Beta0Inicial, Beta2Inicial, Beta3Inicial, Eta1Inicial, Eta2Inicial), 
+                                  f = FuncionObjetivo.SA.Pon, ui=A,
+                                  ci=B, control = list(maxit=1000),
+                                  method = "Nelder-Mead")
   
   #---------------------------------------- Visualización:  
   
@@ -772,40 +763,59 @@ for (i in 1:length(Lista.Bonos)) {
   Fecha.Final <- Fecha.Inicial+years(35)
   
   # Definimos la serie de tiempo:
-  curva.NS <- Curva.NS(X,Fecha.Inicial,Fecha.Final)
-  curva.SA <- Curva.SA(prueba.SA.pon.pso$par,Fecha.Inicial,Fecha.Final)
+  curva.SA <- Curva.SA(parametros.definitivos$par,Fecha.Inicial,Fecha.Final)
   
-  # Grafico de las curvas más reciente:
-  graf.NS <- dygraph(curva.NS,
-                      main = "Curva Cero Cupón - Nelson Siegel", 
-                      xlab = "Fecha", ylab = "Tasa Equivalente Anual",width = "100%") %>% 
-    dySeries("V1", label = "Tasa")
+  # Gráfico de las curvas más reciente:
   graf.SA <- dygraph(curva.SA,
                      main = "Curva Cero Cupón - Svensson Modificado", 
                      xlab = "Fecha", ylab = "Tasa Equivalente Anual",width = "100%") %>% 
     dySeries("V1", label = "Tasa")
   
-  # Visualizamos:
-  graf.NS
-  graf.SA
+  # Cálculo de Error Máximo:
+  errorMax <- FuncionObjetivo.SA.Max(parametros.definitivos$par)
   
   # Guardamos resultados:
   Curvas.Terminadas[[i]] <- list(Parametros = parametros.definitivos$par,
-                                 Error = parametros.definitivos$value,
-                                 Grafico = graf.rho) 
-  
-  # Redefinimos puntos iniciales:
-  Beta2Inicial <- parametros.definitivos$par[[2]]
+                                 ErrorPon = parametros.definitivos$value,
+                                 ErrorMax = errorMax,
+                                 Curva = as.data.frame(curva.SA),
+                                 Grafico = graf.SA)
 }
 
-#---------------------------------------- Grafico de las Tir:
+#---------------------------------------- Gráfico de las Tir - Comparación a corto plazo:
 
-data.corta <- Tir.aplicado %>% select(Fecha.de.Vencimiento, TIR, Diff.semana) %>% 
-  left_join(as.data.frame(curva.SA) %>% mutate(Fecha.de.Vencimiento = as.Date(rownames(as.data.frame(curva.SA))))) %>% 
-  mutate(Curva = V1, TIR = TIR*100, Diff.semana = as.factor(Diff.semana)) %>% select(-V1)
+# Inicialización de la lista de Gráficos:
+TIR.Terminadas <- list()
 
-graf.corta <- ggplot(data=data.corta, aes(x=Fecha.de.Vencimiento, y=TIR,colour = Diff.semana)) +
-  geom_point() +
-  geom_line(aes(y=Curva), color = "red")
+# Función para calcular la TIR de un bono:
+TIR <- function(fila){
+  fila <- Tau.total(fila) %>% arrange(Fecha.Pago)
+  calculo.tir <- function(tir){
+    fila$Precio[1]-sum((rep(fila$Tasa.facial[1],length(fila$Tau))+c(rep(0,length(fila$Tau)-1),1))*(1+tir)^(-fila$Tau/12))
+  }
+  resultado <- nleqslv(0.05, calculo.tir)
+  fila <- fila %>% mutate(TIR = resultado$x)
+  return(fila)
+}
 
-graf.corta
+# Cálculo para cada uno de los meses:
+for (i in 1:length(Lista.Bonos)) {
+ 
+  # Calculamos la TIR para cada bono para observación:
+  Tir.aplicado <- bind_rows(lapply(split(Lista.Bonos[[i]], seq(nrow(Lista.Bonos[[i]]))), TIR)) %>% group_by(Numero.de.Contrato.Largo) %>%
+    mutate(cant= n()) %>% ungroup() %>% filter(cant ==1) %>% 
+    select(-Fecha.Pago) %>% unique()
+  
+  data.corta <- Tir.aplicado %>% select(Fecha.de.Vencimiento, TIR, Diff.semana) %>% 
+    left_join(Curvas.Terminadas[[i]]$Curva %>% mutate(Fecha.de.Vencimiento = as.Date(rownames(Curvas.Terminadas[[i]]$Curva))), by = "Fecha.de.Vencimiento") %>% 
+    mutate(Curva = V1, TIR = TIR*100, Diff.semana = as.factor(Diff.semana)) %>% select(-V1)
+  
+  # Visualizamos el Corto Plazo:
+  graf.corta <- ggplot(data=data.corta, aes(x=Fecha.de.Vencimiento, y=TIR,colour = Diff.semana)) +
+    geom_point() +
+    geom_line(aes(y=Curva), color = "red") +
+    labs(color = "Semana de
+       \r Operación", x = "Fecha de Vencimiento", "TIR") 
+  
+  TIR.Terminadas[[i]] <- graf.corta
+}
