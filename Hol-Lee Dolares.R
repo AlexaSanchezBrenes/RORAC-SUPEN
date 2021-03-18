1505e26579e5ae8f35715222b3afe9a86daba5b7
DesAct12 <isaac.actuario@gmail.com>
2021-03-18 17:53:04 -0600

    Limpieza de forma.

--- Hol-Lee Dolares.R
+++ Hol-Lee Dolares.R
@@ -9,74 +9,74 @@
 # En este módulo se utiliza el árbol binomial del modelo Ho-Lee de tasas de interes aplicado
 # a la curva soberana interpolada linealmente del Tesoro (Dólares).
 
-  # Paquetes Necesarios:
-  library(dplyr)
-  library(dygraphs)
-  library(xts)
-  library(purrr)
-  library(stringr)
-  library(readxl)
-  library(lubridate)
-  Sys.setlocale("LC_TIME", "Spanish")
-  options(stringsAsFactors = FALSE)
+# Paquetes Necesarios:
+library(dplyr)
+library(dygraphs)
+library(xts)
+library(purrr)
+library(stringr)
+library(readxl)
+library(lubridate)
+Sys.setlocale("LC_TIME", "Spanish")
+options(stringsAsFactors = FALSE)
   
-  #---------------------------------------- Carga de Datos:
+#---------------------------------------- Carga de Datos:
   
-  # Historial de Overnight de Estados Unidos: 
-  Overnight <- read.csv("overnightrate.csv",sep=',',dec='.',header = F)
-  Overnight1 <- read.csv("overnightrate (1).csv",sep=',',dec='.',header = F)
-  Overnight2 <- read.csv("overnightrate (2).csv",sep=',',dec='.',header = F)
-  Overnight3 <- read.csv("overnightrate (3).csv",sep=',',dec='.',header = F)
-  Overnight4 <- read.csv("overnightrate (4).csv",sep=',',dec='.',header = F)
-  Overnight5 <- read.csv("overnightrate (5).csv",sep=',',dec='.',header = F)
-  Overnight6 <- read.csv("overnightrate (6).csv",sep=',',dec='.',header = F)
-  Overnight7 <- read.csv("overnightrate (7).csv",sep=',',dec='.',header = F)
-  Overnight <- rbind(Overnight,Overnight1,Overnight2,Overnight3,Overnight4,Overnight5,Overnight6,Overnight7)
-  rm(Overnight1,Overnight2,Overnight3,Overnight4,Overnight5,Overnight6,Overnight7)
-  Overnight <- Overnight[,-3]
-  colnames(Overnight)=c('Fecha','Tasa')
-  Overnight <- Overnight %>% mutate(Tasa=Tasa/100) %>% mutate(TasaS=((1+Tasa/360)^(360/2)-1)*2) %>% mutate(Fecha=as.Date(Fecha, format = '%m/%d/%Y'))
+# Historial de Overnight de Estados Unidos: 
+Overnight <- read.csv("overnightrate.csv",sep=',',dec='.',header = F)
+Overnight1 <- read.csv("overnightrate (1).csv",sep=',',dec='.',header = F)
+Overnight2 <- read.csv("overnightrate (2).csv",sep=',',dec='.',header = F)
+Overnight3 <- read.csv("overnightrate (3).csv",sep=',',dec='.',header = F)
+Overnight4 <- read.csv("overnightrate (4).csv",sep=',',dec='.',header = F)
+Overnight5 <- read.csv("overnightrate (5).csv",sep=',',dec='.',header = F)
+Overnight6 <- read.csv("overnightrate (6).csv",sep=',',dec='.',header = F)
+Overnight7 <- read.csv("overnightrate (7).csv",sep=',',dec='.',header = F)
+Overnight <- rbind(Overnight,Overnight1,Overnight2,Overnight3,Overnight4,Overnight5,Overnight6,Overnight7)
+rm(Overnight1,Overnight2,Overnight3,Overnight4,Overnight5,Overnight6,Overnight7)
+Overnight <- Overnight[,-3]
+colnames(Overnight)=c('Fecha','Tasa')
+Overnight <- Overnight %>% mutate(Tasa=Tasa/100) %>%
+  mutate(TasaS=((1+Tasa/360)^(360/2)-1)*2) %>% mutate(Fecha=as.Date(Fecha, format = '%m/%d/%Y'))
   
-  # Se calcula el delta para generar las proporciones u y d:
-  Overnight <- Overnight %>% mutate(Delta=log((1+Tasa/100/360))*360/12)
+# Se calcula el delta para generar las proporciones u y d:
+Overnight <- Overnight %>% mutate(Delta=log((1+Tasa/100/360))*360/12)
   
-  # Historial de curvas soberanas del Tesoro:
-  Curvas.Tes <- read_excel("tnc_18_22.xls", col_names = FALSE, skip = 5) %>% filter_all(any_vars(!is.na(.)))
-  Curvas.Tes <- Curvas.Tes[, -c(1,2)]
-  nom.col.i <- ymd(paste0("20",str_sub("tnc_18_22.xls", start = 5, end = 6),"-", "01","-","01"))
-  nom.col.f <- ymd(paste0("20",str_sub("tnc_18_22.xls", start = 8, end = 9),"-", "12","-","01"))
-  vec.nom <- seq.Date(from = nom.col.i, to = nom.col.f, by = "month")
-  nombre.col <- vec.nom[1:ncol(Curvas.Tes)]
-  colnames(Curvas.Tes) <- nombre.col
-  Curvas.Tes <- Curvas.Tes %>% mutate(Vencimiento = seq(6,1200,6))
+# Historial de curvas soberanas del Tesoro:
+Curvas.Tes <- read_excel("tnc_18_22.xls", col_names = FALSE, skip = 5) %>% filter_all(any_vars(!is.na(.)))
+Curvas.Tes <- Curvas.Tes[, -c(1,2)]
+nom.col.i <- ymd(paste0("20",str_sub("tnc_18_22.xls", start = 5, end = 6),"-", "01","-","01"))
+nom.col.f <- ymd(paste0("20",str_sub("tnc_18_22.xls", start = 8, end = 9),"-", "12","-","01"))
+vec.nom <- seq.Date(from = nom.col.i, to = nom.col.f, by = "month")
+nombre.col <- vec.nom[1:ncol(Curvas.Tes)]
+colnames(Curvas.Tes) <- nombre.col
+Curvas.Tes <- Curvas.Tes %>% mutate(Vencimiento = seq(6,1200,6))
   
-  #---------------------------------------- Parámetros Generales:
+#---------------------------------------- Parámetros Generales:
   
-  # Cantidad de simulaciones:
-  cant.simu <- 10000
+# Cantidad de simulaciones:
+cant.simu <- 10000
   
-  # Se calcula la varianza mensual:
-  varianza.mensual.dol<-30*var(Overnight$Delta)
+# Se calcula la varianza mensual:
+varianza.mensual.dol<-30*var(Overnight$Delta)
   
-  # Generamos las proporciones de subida y bajada iniciales:
-  u2.dol<-1+sqrt(varianza.mensual.dol) 
-  d2.dol<-1-sqrt(varianza.mensual.dol)
+# Generamos las proporciones de subida y bajada iniciales:
+u2.dol<-1+sqrt(varianza.mensual.dol) 
+d2.dol<-1-sqrt(varianza.mensual.dol)
   
-  # Fijamos el parámetro fijo k del modelo:
-  k <- d2.dol/u2.dol
+# Fijamos el parámetro fijo k del modelo:
+k <- d2.dol/u2.dol
   
-  # Fijamos el tiempo a simular (35 años) en meses:
-  tiempo <- 12*35
+# Fijamos el tiempo a simular (35 años) en meses:
+tiempo <- 12*35
   
-  # Fechas del periodo a simular:
-  Fecha.Inicial <- as.Date("03/01/2020", format = '%m/%d/%Y')
-  Fecha.Final <- Fecha.Inicial+years(tiempo/12)
+# Fechas del periodo a simular:
+Fecha.Inicial <- as.Date("03/01/2020", format = '%m/%d/%Y')
+Fecha.Final <- Fecha.Inicial+years(tiempo/12)
   
   # Datos de tasas compuestas semestralmente observadas:
-  
-  data.tasas <- Curvas.Tes %>% select(as.character(Fecha.Inicial), Vencimiento) %>% 
-    rename(Tasa = as.character(Fecha.Inicial)) %>% mutate(Tasa=Tasa/100)
-  data.tasas <- rbind(c(mean(Overnight$TasaS[which((month(Overnight$Fecha)==month(Fecha.Inicial) & year(Overnight$Fecha)==year(Fecha.Inicial)))]),0),data.tasas)
+data.tasas <- Curvas.Tes %>% select(as.character(Fecha.Inicial), Vencimiento) %>% 
+  rename(Tasa = as.character(Fecha.Inicial)) %>% mutate(Tasa=Tasa/100)
+data.tasas <- rbind(c(mean(Overnight$TasaS[which((month(Overnight$Fecha)==month(Fecha.Inicial) & year(Overnight$Fecha)==year(Fecha.Inicial)))]),0),data.tasas)
   
 
 #---------------------------------------- Funciones del Modelo:
